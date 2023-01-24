ed3ec082ee0eb27e48f7040d95b4477abc7528cb
rishaanray <85553353+rishaanray@users.noreply.github.com>
2023-01-24 18:59:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier= ml5.imageClassifier('mobileNet', modelLoaded );
+}
+
+function modelLoaded(){
+  console.log("console is loaded");
+}
+
+function draw(){
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
+ 
+previous_result= "";
+function gotResult(error, results){
+  if (error){
+    console.log(error);
+  }
+  else {
+    if((results[0].confidence>0.5)&&(previous_result!=results[0].label)){
+      console.log(results);
+      previous_result=results[1].label;
+      synth=window.speechSynthesis;
+      speak_data='object detected is'+results[0].label;
+      utterThis=new SpeechSynthesisUtterance(speak_data);
+      synth.speak(utterThis);
+      document.getElementById("result_object_name").innerHTML=results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+    }
+  }
+}
+
+
+
